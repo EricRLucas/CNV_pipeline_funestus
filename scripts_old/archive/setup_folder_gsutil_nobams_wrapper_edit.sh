5fc0a6cd2738ae167ea1c48439725033bd34f010
EricRLucas <eric.lucas@lstmed.ac.uk>
2024-07-18 09:35:57 +0300

    removed old scripts

--- scripts_old/archive/setup_folder_gsutil_nobams_wrapper_edit.sh
+++
@@ -1,9 +0,0 @@
-≈release=$1
-study_id=$2
-sampleset=$3
-
-workingfolder=~/my_lustre/analysis/${release}_${study_id}
-gsutil_path=gs://vo_afun_release/${release}
-logfolder=$workingfolder/folder_setup_log 
-mkdir -p $logfolder 
-~/my_lustre/cnv_calls/CNV_funestus/scripts/archive/setup_folder_gsutil_nobams_edit.sh $workingfolder $gsutil_path $study_id $sampleset > $logfolder/setup_folder_gsutil.log 2>&1 & 
